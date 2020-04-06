17d0449d1b148fba1260e6240ca1ffd87c6c238a
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-04-06 10:42:22 -0600

    Install @nestjs-modules/mailer
    Setup mailer
    Use maildev as mail server with Docker Compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+  	MailerModule.forRoot({
+  	  transport: {
+  	    host: 'localhost',
+  	    port: 1025,
+  		secure: false, // upgrade later with STARTTLS
+  		auth: {
+  		  user: "mailer",
+  		  pass: "password",
+  		},
+      },
+  	}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

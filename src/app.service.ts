be10c2d82c29c17684b7dd70eb1ebcb03850cff2
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-04-06 11:54:09 -0600

    Send email

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly mailerService: MailerService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  email() {
+    return this
+    .mailerService
+    .sendMail({
+      to: 'john@example.com',
+      from: 'no-reply@localhost',
+      subject: 'Testing Nest Mailermodule with template ✔',
+      text: 'welcome',
+      html: '<b>welcome</b>',
+    })
   }
 }
